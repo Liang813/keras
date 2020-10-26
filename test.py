9d3d5c52eecc6eab4dd28a70dd8994adff97fb42
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-10-26 19:17:59 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import tempfile
+import os
+
+from keras.layers import Input, GRU, CuDNNGRU, TimeDistributed
+from keras.models import Model
+import numpy as np
+
+input2 = Input((4, 6, 10))
+output_cudnn = TimeDistributed(CuDNNGRU(units))(input2)
+output = TimeDistributed(GRU(units, activation='hard_sigmoid', reset_after=True))(input2)
+model_cudnn = Model(input2, output_cudnn)
+model_plain = Model(input2, output)
+
+_, fname = tempfile.mkstemp('.h5')
+model_cudnn.save_weights(fname)
+model_plain.load_weights(fname)
