5b013e94240848e6d536ac4b63843a63ad28995a
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-10-29 14:31:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-import os
-os.environ['KERAS_BACKEND'] = 'tensorflow'
-import keras.backend as K
-from keras.layers import Activation, Conv2D, Input
-from keras.layers.normalization import BatchNormalization
-
-# declare network model with channels first: ERROR
+from keras import backend as K
+from keras.layers import Input, Conv2D, BatchNormalization
 K.set_image_data_format('channels_first')
 input = Input(shape=(3, 1001, 1001), dtype='float32')
 x = Conv2D(filters=64, kernel_size=(3, 3), strides=1, padding='same')(input)
 x = BatchNormalization(axis=1)(x)
-x = Activation('relu')(x)
