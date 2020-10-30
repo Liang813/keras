70c8f77034852845b4c8c7a35ebcecdd750cb0fd
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-10-30 15:55:53 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+
+conv = Conv2D(16, 3, strides=2, padding='same')
+transpose_conv = Conv2DTranspose(1, 3, strides=2, padding='same')
+
+input_a = Input(shape=(23, 23, 1))
+x_conv_a = conv(input_a)
+x_transpose_a = transpose_conv(x_conv_a)
+
+print("(a) Input shape: {}".format(int_shape(input_a)))
+print("(a) Shape after convolution: {}".format(int_shape(x_conv_a)))
+print("(a) Shape after transposed convolution: {}".format(int_shape(x_transpose_a)))
+print()
+
+input_b = Input(shape=(24, 24, 1))
+x_conv_b = conv(input_b)
+x_transpose_b = transpose_conv(x_conv_b)
+
+print("(b) Input shape: {}".format(int_shape(input_b)))
+print("(b) Shape after convolution: {}".format(int_shape(x_conv_b)))
+print("(b) Shape after transposed convolution: {}".format(int_shape(x_transpose_b)))
