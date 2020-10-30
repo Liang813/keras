02a99fd76ee932419985bd4e4ca1ba0dc37f9261
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-10-30 21:55:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
 
+import pandas as pd
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+df = pd.DataFrame(['This is really good',
+				  'Every day is a new day',
+				  ], columns=['text'])
+print(df, end='\n\n')
+MAX_NUM_WORDS = 3
+
+tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+tokenizer.fit_on_texts(df.text)
+sequences = tokenizer.texts_to_sequences(df.text)
+print(sequences, end='\n\n')
+
+padded = pad_sequences(sequences, maxlen=3, padding='post')
+print('After Padding: \n',padded, end='\n\n')
+
+print(tokenizer.word_index)
+print(len(tokenizer.word_index))
