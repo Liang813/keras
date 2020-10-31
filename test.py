2fb0eb57462e73890b70086103615d8903a7973e
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-10-31 09:42:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from keras.layers import Conv2D, Conv2DTranspose
 conv = Conv2D(16, 3, strides=2, padding='same')
 transpose_conv = Conv2DTranspose(1, 3, strides=2, padding='same')
 
-input_a = Input(shape=(23, 23, 1))
+input_a = Input(shape=(12, 12, 16))
 x_conv_a = conv(input_a)
 x_transpose_a = transpose_conv(x_conv_a)
 
@@ -15,7 +15,7 @@ print("(a) Shape after convolution: {}".format(int_shape(x_conv_a)))
 print("(a) Shape after transposed convolution: {}".format(int_shape(x_transpose_a)))
 print()
 
-input_b = Input(shape=(24, 24, 1))
+input_b = Input(shape=(12, 12, 16))
 x_conv_b = conv(input_b)
 x_transpose_b = transpose_conv(x_conv_b)
 
