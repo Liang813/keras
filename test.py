31d549f1350addd63d144d5951529f436616aa57
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-10-31 16:15:17 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+
+a = np.reshape(np.array([0, 0]), (1, 2, 1)) # zero vector has zero length
+b = np.reshape(np.array([0.6, 0.8]), (1, 2, 1))
+
+input_a = Input((2, 1))
+input_b = Input((2, 1))
+
+cos = merge([input_a, input_b], mode='cos', dot_axes=1)
+
+model = Model(input=[input_a, input_b], output=[cos])
+result = model.predict([a, b])
+# before fix: result  == [[[[ nan ]]]]
+# after fix: result == [[[[ 0. ]]]]
