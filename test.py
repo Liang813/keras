399b72c9376e8ddff890abe5ddbd9ed789b4cd66
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-01-01 10:34:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,26 @@
 from keras import Input
 from keras.backend import int_shape
 from keras.layers import Conv2D, Conv2DTranspose
+try:
+  conv = Conv2D(16, 3, strides=2, padding='same')
+  transpose_conv1 = Conv2DTranspose(1, 3, strides=2, padding='same',output_padding=0)
+  transpose_conv2 = Conv2DTranspose(1, 3, strides=2, padding='same',output_padding=1)
 
-conv = Conv2D(16, 3, strides=2, padding='same')
-transpose_conv1 = Conv2DTranspose(1, 3, strides=2, padding='same',output_padding=0)
-transpose_conv2 = Conv2DTranspose(1, 3, strides=2, padding='same',output_padding=1)
+  input_a = Input(shape=(23, 23, 1))
+  x_conv_a = conv(input_a)
+  x_transpose_a = transpose_conv1(x_conv_a)
 
-input_a = Input(shape=(23, 23, 1))
-x_conv_a = conv(input_a)
-x_transpose_a = transpose_conv1(x_conv_a)
+  print("(a) Input shape: {}".format(int_shape(input_a)))
+  print("(a) Shape after convolution: {}".format(int_shape(x_conv_a)))
+  print("(a) Shape after transposed convolution: {}".format(int_shape(x_transpose_a)))
+  print()
 
-print("(a) Input shape: {}".format(int_shape(input_a)))
-print("(a) Shape after convolution: {}".format(int_shape(x_conv_a)))
-print("(a) Shape after transposed convolution: {}".format(int_shape(x_transpose_a)))
-print()
+  input_b = Input(shape=(24, 24, 1))
+  x_conv_b = conv(input_b)
+  x_transpose_b = transpose_conv2(x_conv_b)
 
-input_b = Input(shape=(24, 24, 1))
-x_conv_b = conv(input_b)
-x_transpose_b = transpose_conv2(x_conv_b)
-
-print("(b) Input shape: {}".format(int_shape(input_b)))
-print("(b) Shape after convolution: {}".format(int_shape(x_conv_b)))
-print("(b) Shape after transposed convolution: {}".format(int_shape(x_transpose_b)))
+  print("(b) Input shape: {}".format(int_shape(input_b)))
+  print("(b) Shape after convolution: {}".format(int_shape(x_conv_b)))
+  print("(b) Shape after transposed convolution: {}".format(int_shape(x_transpose_b)))
+except Exception as e:
+  print(str(e))
