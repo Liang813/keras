c9efed8e6fa94853c4f8bc399988b9d5f332e0ee
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-01-05 09:38:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from keras.layers import Input, Convolution3D
-a = Input(shape=(32, 32, 32, 3))
-b = Input(shape=(64, 64, 64, 3))
 
-conv = Convolution3D(16, 3, 3, 3, border_mode='same')
-conved_a = conv(a)
+try:
+  a = Input(shape=(32, 32, 32, 3))
+  b = Input(shape=(64, 64, 64, 3))
 
-# only one input so far, the following will work:
-assert conv.input_shape == (None, 32, 32, 32, 3)
+  conv = Convolution3D(16, 3, 3, 3, border_mode='same')
+  conved_a = conv(a)
 
-conved_b = conv(b)
+  # only one input so far, the following will work:
+  assert conv.input_shape == (None, 32, 32, 32, 3)
+
+  conved_b = conv(b)
+  
+except Exception as e:
+  print(str(e))
