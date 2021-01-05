9c36d123298dc3647321d45e15b3020b1a0014dd
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-01-05 09:53:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ from keras.layers import core, wrappers
 from keras.models import Sequential
 import numpy as np
 
-model = Sequential()
-model.add(wrappers.TimeDistributed(core.Dropout(.5), input_shape=(3, 2)))
-model.compile(optimizer='rmsprop', loss='mse')
-model.predict(np.random.random((10, 3, 2)))
+try:
+  model = Sequential()
+  model.add(wrappers.TimeDistributed(core.Dropout(.5), input_shape=(3, 2)))
+  model.compile(optimizer='rmsprop', loss='mse')
+  model.predict(np.random.random((10, 3, 2)))
+except Exception as e:
+  print(str(e))
