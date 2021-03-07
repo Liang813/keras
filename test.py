72f7f37b532f68119e5da7390900b937f2b5b2fc
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-07 10:03:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import tsensor
 a = Input(shape=(10, 3))
 b = Input(shape=(10, 3))
 add_layer = Add()
-c = add_layer([a, b])
-
 with tsensor.clarify():
-  assert add_layer.output_shape == (None, 10, 3)
+  c = add_layer([a, b])
+assert add_layer.output_shape == (None, 10, 3)
