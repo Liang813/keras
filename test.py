a746edb82106ad8f822995950f5266cc98d9d55a
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-07 09:51:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 from keras.layers import Add, Input
+import tsensor
 
 a = Input(shape=(10, 3))
 b = Input(shape=(10, 3))
 add_layer = Add()
 c = add_layer([a, b])
 
-assert add_layer.output_shape == (None, 10, 3)
+with tsensor.clarify():
+  assert add_layer.output_shape == (None, 10, 3)
