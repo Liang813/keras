c643882ec7f53b220192ee36935913898979f434
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-08 18:24:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from keras.layers import Input, Convolution3D
 
-import tsensor
 
 a = Input(shape=(32, 32, 32, 3))
 b = Input(shape=(64, 64, 64, 3))
@@ -10,6 +9,5 @@ conved_a = conv(a)
 
 # only one input so far, the following will work:
 assert conv.input_shape == (None, 32, 32, 32, 3)
-with tsensor.clarify():
-  conved_b = conv(b)
+conved_b = conv(b)
 
